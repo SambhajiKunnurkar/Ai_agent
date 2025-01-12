49b145157678708e6fdb2883a5dd8fa5160c0f8e
sambhajik <sambhajikunnurkar@gmail.com.com>
2025-01-12 15:30:40 +0530

    server setup

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,23 @@
+import {Router} from 'express';
+import * as userControler from '../controllers/user.controller.js';
+import { body } from 'express-validator';
+import * as authMiddleware from '../middleware/auth.middleware.js'
+const router = Router();
+
+
+router.post('/register',
+    body('email').isEmail().withMessage('email must be valid email'),
+    body('password').isLength({min: 3}).withMessage('password must be at'),
+
+    userControler.creatUserController);
+router.post('/login',
+    body('email').isEmail().withMessage('email must be valid email'),
+    body('password').isLength({min: 3}).withMessage('password must be at'),
+    userControler.loginController
+
+);
+
+
+router.get('/profile', authMiddleware.authUser, userControler.profileController);
+
+export default router;
